05f94427cbb94464f56b3d071f4db956ad01e9e6
Samuel <62902167+Samuelbezerra@users.noreply.github.com>
2023-03-18 19:14:36 -0300

    almost done

---
+++ index.js
@@ -0,0 +1,99 @@
+let currentColor = "#333333"
+let currentMode = "color"
+
+const squareCounter = document.getElementById("square-counter");
+const container = document.getElementById("container")
+const square = document.getElementsByClassName("square")
+const colorSelector = document.getElementById("color-selector")
+const colorButton = document.getElementById("0")
+const rainbowButton = document.getElementById("1")
+const eraserButton = document.getElementById("2")
+const clearButton = document.getElementById("3")
+
+function reloadSquares(size) {
+    for(var i = 0; i<size**2; i++) {
+        var squareDiv = document.createElement("div")
+        squareDiv.classList.add("square")
+        squareDiv.style.width= squareSize +"px"
+        squareDiv.style.height= squareSize +"px"
+        squareDiv.addEventListener("mouseover", changeColor)
+        squareDiv.addEventListener("mousedown", changeColor)
+
+        container.appendChild(squareDiv);
+    }   
+}
+
+colorSelector.oninput = (e)=> currentColor = e.target.value;
+colorButton.onclick = () => {
+    currentMode = "color"
+    colorButton.classList.add("active")
+    rainbowButton.classList.remove("active")
+    eraserButton.classList.remove("active")
+}
+rainbowButton.onclick = () => {
+    currentMode = "rainbow"
+    colorButton.classList.remove("active")
+    rainbowButton.classList.add("active")
+    eraserButton.classList.remove("active")
+}
+eraserButton.onclick = () => {
+    currentMode = "eraser"
+    colorButton.classList.remove("active")
+    rainbowButton.classList.remove("active")
+    eraserButton.classList.add("active")
+}
+clearButton.onclick = () => ""
+
+
+let mouseDown = false
+document.body.onmousedown = () => mouseDown = true
+document.body.onmouseup = () => mouseDown = false
+
+for(var i = 0; i<16**2; i++) {
+    var squareDiv = document.createElement("div")
+    squareDiv.classList.add("square")
+    squareDiv.addEventListener("mouseover", changeColor)
+    squareDiv.addEventListener("mousedown", changeColor)
+
+    container.appendChild(squareDiv)
+}
+
+
+function changeSquare(squareCount) {
+    const squareSize = 400/squareCount; 
+    squareCounter.innerHTML = `${squareCount} x ${squareCount}`
+    container.innerHTML = ""
+
+    for(var i = 0; i<squareCount**2; i++) {
+        var squareDiv = document.createElement("div")
+        squareDiv.classList.add("square")
+        squareDiv.style.width= squareSize +"px"
+        squareDiv.style.height= squareSize +"px"
+        squareDiv.addEventListener("mouseover", changeColor)
+        squareDiv.addEventListener("mousedown", changeColor)
+
+        container.appendChild(squareDiv);
+    }    
+}
+
+function toggleButton (index) {
+    for(var i = 0;i<4;i++) {
+        document.getElementById(`${i}`).classList.remove("active");
+    }
+    const button = document.getElementById(`${index}`)
+    button.classList.add("active")
+}
+
+function changeColor(e) {
+    if(e.type === 'mouseover' && !mouseDown) return
+    if (currentMode === 'color') {
+        e.target.style.backgroundColor = currentColor
+    } else if (currentMode === 'rainbow') {
+        const randomR = Math.floor(Math.random() * 256)
+        const randomG = Math.floor(Math.random() * 256)
+        const randomB = Math.floor(Math.random() * 256)
+        e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`
+    } else if (currentMode === "eraser") {
+        e.target.style.backgroundColor = "white"
+    }
+}
